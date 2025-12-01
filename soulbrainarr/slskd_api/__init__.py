9eaafa5be5912c03272ee73922a684fad0a1342e
Cynopolis <quinn.henthorne@gmail.com>
2025-12-01 14:51:07 -0500

    Auto import new songs into beets
    Added a wait for downloads to complete function
    
    Wrote a beets auto import script
    
    Use the fiven config and database path for beets
    
    Added the rest of the beets functions and an example compose file
    
    Added an init beets function
    
    Added a pip install for beets requirements
    
    done

--- soulbrainarr/slskd_api/__init__.py
+++ soulbrainarr/slskd_api/__init__.py
@@ -1,1 +1,2 @@
-from .__main__ import search_slskd, attempt_download, attempt_each_download
+from .__main__ import search_slskd
+from .downloads import attempt_download, attempt_downloads, wait_for_downloads_to_complete
